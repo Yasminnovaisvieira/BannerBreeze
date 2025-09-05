4926b27b70d230bba9dd466e00089ab41646f4de
Yasminnovaisvieira <Yasminnovaisvieira789@gmail.com>
2025-09-05 11:26:39 -0300

    Banner Breeze

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Banner from "./pages/Banner";
 
 function App() {
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Banner />} /> {/* Rota para a página do Banner */}
+      </Routes>
+    </Router>
   );
 }
 
