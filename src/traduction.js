222771548241ebbe1fcdcfb615a518b3e8685ff2
Yasminnovaisvieira <Yasminnovaisvieira789@gmail.com>
2025-09-06 16:04:07 -0300

    Banner Breeze

--- src/traduction.js
+++ src/traduction.js
@@ -3,7 +3,7 @@ export const textos = {
         subTitulo: "Acompanhe as condições climáticas em tempo real com precisão",
         descricao: "Com o Breeze é possívelmonitorar em tempo real a temperatura e umidade de seus ambientes com a precisão do ESP32 e do ESP8266.Controle dispositivos como ar-condicionado e ventiladores de forma simples e eficiente, garantindo conforto e praticidade. Visualize dados classificados automaticamente e gerencie cada ambiente de acordo com sua necessidade",
         tecnologia: "Tecnologias",
-        time: "Time"
+        time: "Equipe"
     },
     en: {
         subTitulo: "Monitor weather conditions in real time with accuracy",
