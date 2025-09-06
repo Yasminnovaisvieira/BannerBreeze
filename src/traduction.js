b23ab7c9fb9d1608e247c8ea38a9040f9a4f3d46
Yasminnovaisvieira <Yasminnovaisvieira789@gmail.com>
2025-09-06 16:12:59 -0300

    Banner Breeze

--- src/traduction.js
+++ src/traduction.js
@@ -1,7 +1,7 @@
 export const textos = {
     pt: {
         subTitulo: "Acompanhe as condições climáticas em tempo real com precisão",
-        descricao: "Com o Breeze é possível <strong>monitorar em tempo real a temperatura e umidade</strong> de seus ambientes com a precisão do <strong>ESP32</strong> e do <strong>ESP8266</strong>.Controle dispositivos como ar-condicionado e ventiladores de forma <strong>simples</strong> e <strong>eficiente</strong>, garantindo <strong>conforto</strong> e <strong>praticidade</strong>. Visualize <strong>dados classificados automaticamente</strong> e gerencie cada ambiente de acordo com sua necessidade",
+        descricao: "Com o Breeze é possível <strong>monitorar em tempo real a temperatura e umidade</strong> de seus ambientes com a precisão do <strong>ESP32</strong> e do <strong>ESP8266</strong>. Controle dispositivos como ar-condicionado e ventiladores de forma <strong>simples</strong> e <strong>eficiente</strong>, garantindo <strong>conforto</strong> e <strong>praticidade</strong>. Visualize <strong>dados classificados automaticamente</strong> e gerencie cada ambiente de acordo com sua necessidade",
         tecnologia: "Tecnologias",
         time: "Equipe"
     },
